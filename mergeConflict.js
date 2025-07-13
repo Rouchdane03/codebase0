2d66bffcb74cec5e2ba9845a2bd6e2911216e1e4
Rouchdane MOUDJIBOU <114878268+Rouchdane03@users.noreply.github.com>
2025-07-13 20:08:12 +0200

    B for merge conflict (#8)
    * feat: creating a js function for adding 2 numbers
    
    * Fixed add function  and subtract function
    
    * add back semicolon
    
    * Update mergeConflict.js
    
    * export add and substract
    
    * refacto mergeConflict.js with nice function declaration
    
    * add divide function
    
    * Update mergeConflict.js
    
    * add divide in export
    
    * Update substract mergeConflict.js
    
    * Update amoved.txt
    
    ---------
    
    Co-authored-by: rdissDev <116728891+rdissDev@users.noreply.github.com>

---
+++ mergeConflict.js
@@ -0,0 +1,14 @@
+ // this function add two numbers
+const add = (n1,n2)=> n1+n2;
+
+const substract = (n1,n2)=> n1-n2;
+ 
+const divide = (n1,n2)=> {
+  if(n2==0){
+   throw Error("cannot divide by 0");
+  }
+
+  return n1/n2;
+}
+
+export {add, substract, divide}
