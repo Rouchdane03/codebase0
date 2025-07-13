f49ae1ad5b747069f5ae0564278f013854830a46
rdissDev <116728891+rdissDev@users.noreply.github.com>
2025-07-13 19:40:21 +0200

    Update mergeConflict.js

--- mergeConflict.js
+++ mergeConflict.js
@@ -4,7 +4,11 @@ const add = (n1,n2)=> n1+n2;
 const soustraction = (n1,n2)=> n1-n2;
  
 const divide = (n1,n2)=> {
+  if(n2==0){
+   throw Error("cannot divide by 0");
+  }
+
   return n1/n2;
 }
 
-export {add, substract}
+export {add, soustraction, divde}
