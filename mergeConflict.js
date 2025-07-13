827037734589a502a6933c2a5e341ca0171dfc2f
rdissDev <116728891+rdissDev@users.noreply.github.com>
2025-07-13 19:23:04 +0200

    refacto mergeConflict.js with nice function declaration

--- mergeConflict.js
+++ mergeConflict.js
@@ -1,10 +1,6 @@
  // this function add two numbers
-const add = (n1,n2)=> {
-  return n1+n2;
-};
+const add = (n1,n2)=> n1+n2;
 
-const soustraction = (n1,n2)=> {
-  return n1-n2
-}
-
+const soustraction = (n1,n2)=> n1-n2;
+ 
 export {add, substract}
