40a553d39b279bc6f6ab659e39f1f0a385fae4c4
rdissDev <116728891+rdissDev@users.noreply.github.com>
2025-07-13 17:07:19 +0200

    Fixed add function  and subtract function

--- mergeConflict.js
+++ mergeConflict.js
@@ -1,4 +1,8 @@
+ // this function add two numbers
 const add = (n1,n2)=> {
-  // this function add two numbers
-  return n1-n2;
-};
+  return n1+n2
+}
+
+const substract = (n1,n2)=> {
+  return n1-n2
+}
