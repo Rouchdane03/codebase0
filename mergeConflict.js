fed508aa2263b9fefe6be1642997a326202db07c
Rouchdane MOUDJIBOU <114878268+Rouchdane03@users.noreply.github.com>
2025-07-13 19:51:02 +0200

    Update substract mergeConflict.js

--- mergeConflict.js
+++ mergeConflict.js
@@ -1,7 +1,7 @@
  // this function add two numbers
 const add = (n1,n2)=> n1+n2;
 
-const soustraction = (n1,n2)=> n1-n2;
+const substract = (n1,n2)=> n1-n2;
  
 const divide = (n1,n2)=> {
   if(n2==0){
